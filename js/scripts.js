4a13e36f1d751d79d905f7847f1c66308b83760c
Colin Bloom <Colinjbloom113@gmail.com>
2017-03-03 08:55:40 -0800

    initail commmit, html template with other documents linked, create customer object

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+//business logic
+function Customer(name, zip, pizzas, discount, order) {
+  this.name = name;
+  this.zip = zip;
+  this.pizzas = pizzas;
+  this.discount = discount;
+  this.order = order;
+}
+
+//user interface logic
+$(document).ready(function) {
+
+}
